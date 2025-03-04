35666d472b7b4fb99eb2c619a35cb9d93305489c
CheolJe <me@falsy.me>
2025-03-04 12:04:03 +0900

    feat: Remove unnecessary methods

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import LDate from "./date/LDate"
 
-export { asyncMap } from "./array/asyncMap"
-export { groupAndSort } from "./array/groupAndSort"
-export { groupBy } from "./array/groupBy"
 export { multiFilter } from "./array/multiFilter"
+
 export { ILightKitDate, IDateParts } from "./interfaces/date"
 export { LDate }
